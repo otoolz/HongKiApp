99572997b3bf99698c18aaaba5d520ba52c89a48
otoolz <otool.hf@gmail.com>
2022-04-27 23:57:59 +0900

    create xcode project

---
+++ Hongki/Hongki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hongki
+//
+//  Created by KiWoong Hong on 2022/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hongki/Hongki/HongkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HongkiApp.swift
+//  Hongki
+//
+//  Created by KiWoong Hong on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct HongkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
